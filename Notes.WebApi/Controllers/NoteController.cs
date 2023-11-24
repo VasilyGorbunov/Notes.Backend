037aedb05f44733c326245d2e2807fa9216787af
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-24 16:37:56 +0300

    v-07 9:53

---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Queries.GetNoteList;
+
+namespace Notes.WebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery { 
+                UserId = UserId
+            };
+
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,20 @@
+using Notes.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using(var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<NotesDbContext>();
+        DbInitializer.Initialize(context);
+    } catch(Exception exception)
+    {
+
+    }
+}
+
 
 app.Run();
